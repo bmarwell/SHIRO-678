39195076f1fb508a8d8cfdd48499af4a3b723597
Brian Demers <bdemers@apache.org>
2020-03-12 13:01:53 -0400

    Use JAX-RS feature instead of servlet filter

--- src/main/java/io/github/bmhm/shiro/shiro678/RestApplication.java
+++ src/main/java/io/github/bmhm/shiro/shiro678/RestApplication.java
@@ -1,8 +1,20 @@
 package io.github.bmhm.shiro.shiro678;
 
+import org.apache.shiro.web.jaxrs.ShiroFeature;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/")
 public class RestApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(ShiroFeature.class);
+        classes.add(JaxRsEndpoint.class);
+        return classes;
+    }
 }
